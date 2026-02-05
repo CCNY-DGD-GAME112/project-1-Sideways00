5d283a987ddab0bc1bbd11c08e20fd0f4948a000
Sideways00 <anthony.araujo782@gmail.com>
2026-02-05 14:23:53 -0500

    Progress2

--- Assets/HW3/HwAssets/HW3EnemyCollision.cs
+++ Assets/HW3/HwAssets/HW3EnemyCollision.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class HW3EnemyCollision : MonoBehaviour
 {
     // Collision checking
-    private void OnTriggerEnter2D(Collider2D collision) {
+    public void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.tag == "Player") { // Checking if the enemy is colliding with the player
             Destroy(collision.gameObject); // destroying the player if its the player
         }
